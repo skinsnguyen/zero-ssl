1d3afa814c8fe53f55872fa423121692ac952589
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2023-06-19 15:40:24 +0700

    Update zerossl.sh

--- zerossl.sh
+++ zerossl.sh
@@ -20,6 +20,6 @@ read -p "Nhap path  : " pathssl
 wget -O -  https://get.acme.sh | sh -s email={domainssl}   
 source ~/.bashrc
 
-acme.sh --issue --webroot ~/${pathssl} -d ${domainssl} -d www.${domainssl} --force
-acme.sh --deploy --deploy-hook cpanel_uapi --domain ${domainssl} --domain www.${domainssl}  
+./acme.sh --issue --webroot ~/${pathssl} -d ${domainssl} -d www.${domainssl} --force
+./acme.sh --deploy --deploy-hook cpanel_uapi --domain ${domainssl} --domain www.${domainssl}  
 rm -f ${pwdd}/zerossl.sh
