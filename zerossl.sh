0e216d75e18bebc5e06270efcd640bf4add5a948
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2023-06-19 17:18:45 +0700

    Update zerossl.sh

--- zerossl.sh
+++ zerossl.sh
@@ -3,6 +3,7 @@
 ## Last Updated : 19/06/2023
 ## Company : AZDIG
 pwdd=`pwd`
+homeuser=$(echo $pwd)
 cat << "EOF"
  _   _                                          _   _
 | \ | | __ _ _   _ _   _  ___ _ __             | \ | | __ _ _ __ ___
@@ -12,7 +13,7 @@ cat << "EOF"
        |___/       |___/
 
 EOF
-
+homeuser=$(echo $PWD)
 
 echo "! Kich Hoat SSL  !"
 read -p "Nhap domain  :" domainssl
@@ -20,6 +21,6 @@ read -p "Nhap path  : " pathssl
 wget -O -  https://get.acme.sh | sh -s email={domainssl}   
 source ~/.bashrc
 
-./acme.sh --issue --webroot ~/${pathssl} -d ${domainssl} -d www.${domainssl} --force
-./acme.sh --deploy --deploy-hook cpanel_uapi --domain ${domainssl} --domain www.${domainssl}  
+$homeuser/.acme.sh --issue --webroot ~/${pathssl} -d ${domainssl} -d www.${domainssl} --force
+$homeuser/.acme.sh --deploy --deploy-hook cpanel_uapi --domain ${domainssl} --domain www.${domainssl}  
 rm -f ${pwdd}/zerossl.sh
